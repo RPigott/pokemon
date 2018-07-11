606b409e10204e58c7af29578f1848fe909849fc
rpigott <rpigott@berkeley.edu>
2018-07-11 15:36:29 -0700

    Removed trailing +

--- post.sql
+++ post.sql
@@ -8,30 +8,30 @@ cluster species using species_pkey;
 
 -- Make convenient species table
 CREATE MATERIALIZED VIEW species_en AS
-  SELECT DISTINCT sp.id,                                                 +
-     sp.name_en AS name,                                                 +
-     fm.name_en AS form,                                                 +
-     t1.name_en AS type1,                                                +
-     t2.name_en AS type2,                                                +
-     sp.hp,                                                              +
-     sp.atk,                                                             +
-     sp.def,                                                             +
-     sp.spa,                                                             +
-     sp.spd,                                                             +
-     sp.spe,                                                             +
-     (((((sp.hp + sp.atk) + sp.def) + sp.spa) + sp.spd) + sp.spe) AS bst,+
-     ab1.name_en AS ability1,                                            +
-     ab2.name_en AS ability2,                                            +
-     ah.name_en AS hidden_ability,                                       +
-     eg1.name_en AS egg_group1,                                          +
-     eg2.name_en AS egg_group2                                           +
-    FROM ((((((((species sp                                              +
-      JOIN forms fm ON ((sp.id = fm.species)))                           +
-      JOIN types t1 ON ((sp.type1 = t1.id)))                             +
-      FULL JOIN types t2 ON ((sp.type2 = t2.id)))                        +
-      JOIN egg_groups eg1 ON ((sp.egg_group1 = eg1.id)))                 +
-      FULL JOIN egg_groups eg2 ON ((sp.egg_group2 = eg2.id)))            +
-      JOIN abilities ab1 ON ((sp.ability1 = ab1.id)))                    +
-      JOIN abilities ab2 ON ((sp.ability2 = ab2.id)))                    +
-      JOIN abilities ah ON ((sp.ability_hidden = ah.id)))                +
+  SELECT DISTINCT sp.id,                                                 
+     sp.name_en AS name,                                                 
+     fm.name_en AS form,                                                 
+     t1.name_en AS type1,                                                
+     t2.name_en AS type2,                                                
+     sp.hp,                                                              
+     sp.atk,                                                             
+     sp.def,                                                             
+     sp.spa,                                                             
+     sp.spd,                                                             
+     sp.spe,                                                             
+     (((((sp.hp + sp.atk) + sp.def) + sp.spa) + sp.spd) + sp.spe) AS bst,
+     ab1.name_en AS ability1,                                            
+     ab2.name_en AS ability2,                                            
+     ah.name_en AS hidden_ability,                                       
+     eg1.name_en AS egg_group1,                                          
+     eg2.name_en AS egg_group2                                           
+    FROM ((((((((species sp                                              
+      JOIN forms fm ON ((sp.id = fm.species)))                           
+      JOIN types t1 ON ((sp.type1 = t1.id)))                             
+      FULL JOIN types t2 ON ((sp.type2 = t2.id)))                        
+      JOIN egg_groups eg1 ON ((sp.egg_group1 = eg1.id)))                 
+      FULL JOIN egg_groups eg2 ON ((sp.egg_group2 = eg2.id)))            
+      JOIN abilities ab1 ON ((sp.ability1 = ab1.id)))                    
+      JOIN abilities ab2 ON ((sp.ability2 = ab2.id)))                    
+      JOIN abilities ah ON ((sp.ability_hidden = ah.id)))                
    ORDER BY sp.id;
